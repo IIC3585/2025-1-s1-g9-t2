b395c066751c2ae59f21c217e3e9991f68e1771e
Tu Nombre <tu_email@example.com>
2025-04-20 19:43:14 -0400

    deploy borrado

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: 'frontend',
   publicDir: '../public',
-  base: '/2025-1-s1-g9-t2/', // <-- importante para GitHub Pages
   build: {
     outDir: '../dist',
     emptyOutDir: true,
