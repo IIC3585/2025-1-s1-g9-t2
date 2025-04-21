f7136c8cb6ae1d120c4c5ed5be3a17a7ccad359a
Stomicicp <stomicicp@uc.cl>
2025-04-21 01:19:51 -0400

    Updates

--- sw.js
+++ sw.js
@@ -8,14 +8,14 @@ self.addEventListener('install', (event) => {
           const base = '/2025-1-s1-g9-t2/';
   
           await cache.addAll([
-            // `${base}`,
-            // `${base}index.html`,
-            // `${base}app.js`,
-            // `${base}style.css`,
-            // `${base}camIcon192.png`,
-            // `${base}camIcon512.png`,
-            // `${base}camIcon512sinfondo2.png`,
-            // `${base}manifest.json`,
+            `${base}`,
+            `${base}index.html`,
+            `${base}app.js`,
+            `${base}style.css`,
+            `${base}camIcon192.png`,
+            `${base}camIcon512.png`,
+            `${base}camIcon512sinfondo2.png`,
+            `${base}manifest.json`,
             `${base}iconLogo.png`
           ]);
         } catch (error) {
@@ -67,3 +67,26 @@ async function cacheSearch(event) {
 }
 
 // Firebase Cloud Messaging (FCM)
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAGHLS1hwXIu7v8EYGHqc320wdwmfZpkj0",
+  authDomain: "pwa-g9-2025.firebaseapp.com",
+  projectId: "pwa-g9-2025",
+  storageBucket: "pwa-g9-2025.firebasestorage.app",
+  messagingSenderId: "619074453275",
+  appId: "1:619074453275:web:853ca8640be57379015dbe",
+  measurementId: "G-ED9XNMQSBZ"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Mensaje en background:', payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/2025-1-s1-g9-t2/iconLogo.png', // o cualquier ícono que tengas
+  });
+});
