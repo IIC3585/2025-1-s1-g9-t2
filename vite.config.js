543bf75ee61032f6518ca190f97f081dd1992922
Stomicicp <stomicicp@uc.cl>
2025-04-20 22:36:04 -0400

    deploy vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: 'frontend',
   publicDir: '../public',
+  base: '/2025-1-s1-g9-t2/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
